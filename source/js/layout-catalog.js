e3a7c739efd92dcd0d44c615fa6897ec60daf750
Ivan <proekt.energetika@gmail.com>
2018-10-29 20:26:11 +0300

    Разметил и стилизовал страницу design

---
+++ source/js/layout-catalog.js
@@ -0,0 +1,56 @@
+const layoutArray = [
+  {
+    name: 'Ванкувер',
+    space: 61.08,
+    bedrooms: 1,
+    commissioned: true,
+    finalDate: '',
+    plan_1x: 'img/design__selection-item--1@1x.png',
+    plan_2x: 'img/design__selection-item--1@2x.png'
+  },
+  {
+    name: 'Дубай',
+    space: 69.05,
+    bedrooms: 1,
+    commissioned: true,
+    finalDate: '',
+    plan_1x: 'img/design__selection-item--2@1x.png',
+    plan_2x: 'img/design__selection-item--2@2x.png'
+  },
+  {
+    name: 'Лас-Вегас',
+    space: 60.70,
+    bedrooms: 1,
+    commissioned: false,
+    finalDate: 'I кв. 2019',
+    plan_1x: 'img/design__selection-item--3@1x.png',
+    plan_2x: 'img/design__selection-item--3@2x.png'
+  },
+  {
+    name: 'Рио',
+    space: 58.89,
+    bedrooms: 1,
+    commissioned: false,
+    finalDate: 'II кв. 2019',
+    plan_1x: 'img/design__selection-item--4@1x.png',
+    plan_2x: 'img/design__selection-item--4@2x.png'
+  },
+  {
+    name: 'Сидней',
+    space: 67.54,
+    bedrooms: 1,
+    commissioned: true,
+    finalDate: '',
+    plan_1x: 'img/design__selection-item--5@1x.png',
+    plan_2x: 'img/design__selection-item--5@2x.png'
+  },
+  {
+    name: 'Токио',
+    space: 63.61,
+    bedrooms: 1,
+    commissioned: true,
+    finalDate: '',
+    plan_1x: 'img/design__selection-item--6@1x.png',
+    plan_2x: 'img/design__selection-item--6@2x.png'
+  }
+];
