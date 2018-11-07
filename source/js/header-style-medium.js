9069fd63ca8f59e18ecb5318b0710ac66e112f17
Ivan <proekt.energetika@gmail.com>
2018-11-07 18:38:25 +0300

    Меняю цвет бокового меню динамически

---
+++ source/js/header-style-medium.js
@@ -0,0 +1,39 @@
+'use strict';
+
+(function () {
+  const header = document.querySelector('.header');
+  const langs = header.querySelectorAll('.language__link');
+  const socials = header.querySelectorAll('.social__link');
+
+  function toggleHeaderBackground () {
+    if (window.pageYOffset >= 230) {
+      header.classList.add('header-dark');
+    } else {
+      if (header.classList.contains('header-dark')) {
+        header.classList.remove('header-dark');
+      }
+    }
+  }
+
+  function toggleHeaderSidemenuColor () {
+    langs.forEach((item, index) => {
+      if (window.pageYOffset >= 15 + 25 * index) {
+        item.style.color = 'rgb(123, 101, 66)';
+      } else {
+        item.style.color = 'rgb(255, 255, 255)';
+      }
+    })
+
+    socials.forEach(item => item.style.color = 'rgb(123, 101, 66)');
+  }
+
+
+  toggleHeaderBackground();
+  toggleHeaderSidemenuColor();
+
+  document.addEventListener('scroll', function () {
+    console.log(window.pageYOffset);
+    toggleHeaderBackground();
+    toggleHeaderSidemenuColor();
+  })
+})();
---
+++ source/js/header-style-small.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function () {
+  const header = document.querySelector('.header');
+  const langs = header.querySelectorAll('.language__link');
+  const socials = header.querySelectorAll('.social__link');
+
+  function toggleHeaderBackground () {
+    if (window.pageYOffset >= 40) {
+      header.classList.add('header-dark');
+    } else {
+      if (header.classList.contains('header-dark')) {
+        header.classList.remove('header-dark');
+      }
+    }
+  }
+
+  function toggleHeaderSidemenuColor () {
+    langs.forEach(item => item.style.color = 'rgb(123, 101, 66)');
+    socials.forEach(item => item.style.color = 'rgb(123, 101, 66)');
+  }
+
+  toggleHeaderBackground();
+  toggleHeaderSidemenuColor();
+
+  document.addEventListener('scroll', function () {
+    console.log(window.pageYOffset);
+    toggleHeaderBackground();
+  })
+})();
